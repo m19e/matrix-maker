d2fcb3f32003089590ad1a6fc8b3601a9df9e0c3
m19e <nexus.me7290@gmail.com>
2022-04-03 21:48:28 +0900

    Set default theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html data-theme="bumblebee">
+    <Html>
       <Head />
       <body>
         <Main />
