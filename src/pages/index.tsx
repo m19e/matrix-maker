3681b84518c055eafca347a0db4b5978e0546a91
m19e <nexus.me7290@gmail.com>
2022-04-05 22:42:29 +0900

    Add matrix-maker page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,23 @@
-import TopPage from "@/components/page/Top/"
-export default TopPage
+import Head from "next/head"
+import type { NextPage } from "next"
+
+import { MatrixMaker } from "@/components/templates/MatrixMaker"
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next & daisyUI</title>
+        <meta
+          name="description"
+          content="A template for prototyping with Next.js and daisy UI"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <MatrixMaker />
+    </>
+  )
+}
+
+export default Page
