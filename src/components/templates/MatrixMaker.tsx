2c29c411bf936836adeea81d7190d02c5fa2293b
m19e <nexus.me7290@gmail.com>
2022-04-07 02:02:37 +0900

    Set dynamic imported canvas

--- src/components/templates/MatrixMaker.tsx
+++ src/components/templates/MatrixMaker.tsx
@@ -1,8 +1,14 @@
+import dynamic from "next/dynamic"
 import type { VFC } from "react"
+
+const Canvas = dynamic(import("./Canvas"), {
+  ssr: false,
+})
 
 export const MatrixMaker: VFC = () => {
   return (
     <div className="flex flex-col items-center">
+      <Canvas />
       <div className="p-10 bg-base-100 text-base-content">
         <button className="btn">button</button>
         <button className="btn btn-primary">button</button>
