8903c79bc9e45bac41e5f7c93ea1ab9d87cbb0b9
m19e <nexus.me7290@gmail.com>
2022-04-08 21:53:49 +0900

    Shift buttons to canvas container

--- src/components/templates/MatrixMaker.tsx
+++ src/components/templates/MatrixMaker.tsx
@@ -9,12 +9,6 @@ export const MatrixMaker: VFC = () => {
   return (
     <div className="flex flex-col items-center">
       <Canvas />
-      <div className="p-10 bg-base-100 text-base-content">
-        <button className="btn">button</button>
-        <button className="btn btn-primary">button</button>
-        <button className="btn btn-secondary">button</button>
-        <button className="btn btn-accent">button</button>
-      </div>
     </div>
   )
 }
