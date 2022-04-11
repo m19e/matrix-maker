d4327ad6a51d3f3e72ec11cd97a9a24ac081c643
m19e <nexus.me7290@gmail.com>
2022-04-11 18:28:04 +0900

    Remove free-drawing canvas demo

--- src/components/templates/MatrixMaker.tsx
+++ src/components/templates/MatrixMaker.tsx
@@ -1,9 +1,6 @@
 import dynamic from "next/dynamic"
 import type { VFC } from "react"
 
-const Canvas = dynamic(import("./Canvas"), {
-  ssr: false,
-})
 const Images = dynamic(import("./Images"), {
   ssr: false,
 })
@@ -11,7 +8,6 @@ const Images = dynamic(import("./Images"), {
 export const MatrixMaker: VFC = () => {
   return (
     <div className="flex flex-col items-center">
-      <Canvas />
       <Images />
     </div>
   )
