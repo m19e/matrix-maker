b05b98366098d4a0d2c19b9aa497c8bf6bd80de4
m19e <nexus.me7290@gmail.com>
2022-04-15 01:45:50 +0900

    Recieve initial size

--- src/hooks/useElementSize.ts
+++ src/hooks/useElementSize.ts
@@ -8,24 +8,20 @@ interface Size {
   height: number
 }
 
-export const useElementSize = <T extends HTMLElement = HTMLDivElement>(): [
-  (node: T | null) => void,
-  Size
-] => {
+export const useElementSize = <T extends HTMLElement = HTMLDivElement>(
+  initialSize: Size
+): [(node: T | null) => void, Size] => {
   // Mutable values like 'ref.current' aren't valid dependencies
   // because mutating them doesn't re-render the component.
   // Instead, we use a state as a ref to be reactive.
   const [ref, setRef] = useState<T | null>(null)
-  const [size, setSize] = useState<Size>({
-    width: 0,
-    height: 0,
-  })
+  const [size, setSize] = useState<Size>(initialSize)
 
   // Prevent too many rendering using useCallback
   const handleSize = useCallback(() => {
     setSize({
-      width: ref?.offsetWidth || 0,
-      height: ref?.offsetHeight || 0,
+      width: ref?.offsetWidth || initialSize.width,
+      height: ref?.offsetHeight || initialSize.height,
     })
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
