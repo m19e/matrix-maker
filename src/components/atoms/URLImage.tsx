0ac2c0d5d70303c30317b207ccaa0c954c14dea3
m19e <nexus.me7290@gmail.com>
2022-05-06 23:43:42 +0900

    Add delete-image button

--- src/components/atoms/URLImage.tsx
+++ src/components/atoms/URLImage.tsx
@@ -1,5 +1,5 @@
 import type { VFC } from "react"
-import { Image as KImage } from "react-konva"
+import { Image as KImage, Line } from "react-konva"
 import useImage from "use-image"
 
 import { ImagePropsWithHandler } from "@/types"
@@ -7,5 +7,17 @@ import { ImagePropsWithHandler } from "@/types"
 export const URLImage: VFC<ImagePropsWithHandler> = ({ url, ...props }) => {
   const [image] = useImage(url)
 
-  return <KImage {...props} image={image} draggable _useStrictMode />
+  return (
+    <>
+      <Line
+        points={[props.x, props.y - 16, props.x + 16, props.y]}
+        stroke={"gray"}
+      />
+      <Line
+        points={[props.x, props.y, props.x + 16, props.y - 16]}
+        stroke={"gray"}
+      />
+      <KImage {...props} image={image} draggable _useStrictMode />
+    </>
+  )
 }
