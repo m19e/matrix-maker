d44aa6d4f04c8b763176dd1991600388714aecd8
m19e <nexus.me7290@gmail.com>
2022-05-14 17:33:26 +0900

    Extract image-size-select to file

---
+++ src/components/molecules/ImageSizeSelect.tsx
@@ -0,0 +1,30 @@
+import type { ChangeEvent, VFC } from "react"
+
+const SIZES = ["XS", "S", "M", "L", "LL", "3L"]
+const OPTIONS = SIZES.map((size, i) => ({ size, value: 1 + i * 0.2 })).map(
+  (opt) => ({ ...opt, label: `${opt.value * 100}% ${opt.size}` })
+)
+
+interface Props {
+  onChange: (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => void
+}
+
+export const ImageSizeSelect: VFC<Props> = ({ onChange }) => {
+  return (
+    <select
+      className="flex-1 max-w-xs select-sm select select-bordered"
+      onChange={onChange}
+    >
+      <option disabled selected>
+        IMAGE SIZE
+      </option>
+      {OPTIONS.map((option) => (
+        <option key={option.label} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  )
+}
